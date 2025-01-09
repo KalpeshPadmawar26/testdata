711c5f993a5de8aa291017dcf924976b0482825d
Kalpesh Padmawar <111654076+KalpeshPadmawar26@users.noreply.github.com>
2025-01-09 12:34:59 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
   public static void main(String[] args){
     int a= 10;
     int b = 20;
-    System.out.println("Addition : "+(a+b))
+    System.out.println("Addition : "+(a+b));
+    System.out.println("multiplication : "+(a*b));     
   }
 }
